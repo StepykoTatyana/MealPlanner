d7bc0adeb1397223fd601d14ccf684733a868e3e
Stepyko Tatyana <SunnyTanya93@mail.ru>
2023-03-28 21:52:17 +0700

    Initial commit

---
+++ src/main/java/org/example/Meal.java
@@ -0,0 +1,37 @@
+package mealplanner;
+
+public class Meal {
+    String category;
+    String name;
+    String[] ingredients;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String[] getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String[] ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Meal(String category, String name, String[] ingredients) {
+        this.category = category;
+        this.name = name;
+        this.ingredients = ingredients;
+    }
+}
